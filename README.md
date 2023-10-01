5f11f7f87af8dfb5046372b89c1da74950399da8
Ichinga-Samuel <ichingasamuel@gmail.com>
2023-10-01 11:15:46 +0100

    combine in one class

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # Minimum Spanning Tree Algorithms
 
 Kruskal and Prim Minimum Spanning Tree Algorithms written in Python Object-Oriented Programming(OOP) style.
-Accepts the input graph as a dictionary with keys representing the edges of the graph and, the values their corresponding weight.\
-The vertices that make up an edge in the dictionary should be separated by a single character string like a '-'.
+Accepts the input graph as a dictionary.
 
----
-
-## Example
+### Example
 ```python
-from prim import Prim
+
+# the graph as a dictionary of edges and their weight
 graph = {'1-2': 853.40, '2-3': 160, '2-23': 933.9, '3-4': 170, '3-5': 158.7, '5-6': 142.8, '4-8': 256.7, '6-7': 68.7,
          '7-8': 27.2, '8-9': 142.7, '7-10': 36.8, '10-11': 27.2, '8-11': 36.8, '11-12': 126, '12-13': 208.2, '12-14': 363.6,
          '14-15': 118, '13-16': 328.2, '15-16': 150, '14-17': 100, '15-18': 100, '18-19': 100, '18-17': 118, '16-19': 115,
@@ -16,43 +14,33 @@ graph = {'1-2': 853.40, '2-3': 160, '2-23': 933.9, '3-4': 170, '3-5': 158.7, '5-
          '35-34': 170, '27-34': 115, '34-36': 235, '36-33': 72, '33-26': 574, '25-24': 140, '24-23': 100, '23-22': 100,
          '22-31': 371.35, '24-30': 100, '23-29': 100, '29-30': 100, '29-32': 170, '32-33': 285, '32-31': 100, '17-20': 115}
 
-Prim(graph)
+graph = Graph(graph=graph)
+# use build edges because of the type of graph
+graph.build_edges()
+prim_mst = graph.prim()
+kruskal_mst = graph.kruskal()
+prim_mst.show()
+kruskal_mst.show()
 
-"""{ '1-2': 853.4,
-  '10-11': 27.2,
-  '11-12': 126,
-  '12-13': 208.2,
-  '13-16': 328.2,
-  '14-15': 118,
-  '14-17': 100,
-  '15-18': 100,
-  '16-19': 115,
-  '17-20': 115,
-  '18-19': 100,
-  '18-21': 152.8,
-  '2-3': 160,
-  '21-26': 170,
-  '23-22': 100,
-  '23-29': 100,
-  '24-23': 100,
-  '24-30': 100,
-  '25-24': 140,
-  '26-25': 84.2,
-  '26-27': 130,
-  '27-28': 170,
-  '27-34': 115,
-  '28-35': 115,
-  '29-32': 170,
-  '3-4': 170,
-  '3-5': 158.7,
-  '32-31': 100,
-  '34-36': 235,
-  '36-33': 72,
-  '5-6': 142.8,
-  '6-7': 68.7,
-  '7-10': 36.8,
-  '7-8': 27.2,
-  '8-9': 142.7}
-Total Weight: 5151.9"""
+# Another type of input
+graph1 = {
+    0: [(1, 4), (7, 8), ],
+    1: [(0, 4), (7, 11), (2, 8)],
+    2: [(1, 8), (8, 2), (5, 4), (3, 7)],
+    3: [(2, 7), (4, 9), (5, 14)],
+    4: [(3, 9), (5, 10)],
+    5: [(2, 4), (3, 14), (6, 2), (4, 10)],
+    6: [(7, 1), (5, 2), (8, 6)],
+    7: [(0, 8), (1, 11), (8, 7), (6, 1)],
+    8: [(2, 2), (6, 6), (7, 7)]
+}
 
+graph1 = Graph(data=graph1)
+# use another style to build the graph
+graph1.build()
+prim_mst = graph1.mst()
+kruskal_mst = graph.kruskal()
+
+prim_mst.show()
+kruskal_mst.show()
 ```
--- graph.py
+++ graph.py
@@ -1,109 +1,218 @@
-from typing import List, Dict
+from functools import total_ordering
+from pprint import pprint as pp
 
 
-class Edge:
-    def __init__(self, vertex1, vertex2, weight):
-        self.vertex1 = vertex1
-        self.vertex2 = vertex2
-        self.weight = weight
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.vertex1}, {self.vertex2}, {self.weight})"
+class Vertex:
+    def __init__(self, name):
+        self.name = str(name)
 
     def __eq__(self, other):
-        return self.weight == other.weight
+        return self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name})"
+
+
+@total_ordering
+class Edge:
+    def __init__(self, *, start: Vertex, end: Vertex, weight=None, direction=0):
+        """A class representing an edge in a graph. If direction is 0. Then the edge is not directed.
+        1 is a positive direction from start to end and -1 the reverse
+        """
+        self.start = start
+        self.end = end
+        self.weight = weight
+        self.direction = direction
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
 
     def __lt__(self, other):
         return self.weight < other.weight
 
+    def __le__(self, other):
+        return self.weight <= other.weight
+
     def __hash__(self):
-        return hash((*self.vertices, self.weight))
+        return hash(self.start) ^ hash(self.end) ^ hash(self.direction) ^ hash(self.weight)
 
-    def __contains__(self, item):
-        return item in self.vertices
+    def __str__(self):
+        return f'{self.start}<-->{self.end}: {self.weight}'
 
-    @property
-    def vertices(self):
-        return self.vertex1, self.vertex2
+    def __repr__(self):
+        return f'Edge(start={self.start}, end={self.end}, weight={self.weight}, direction={self.direction})'
 
-    def difference(self, vertex1):
-        return self.vertex2 if vertex1 == self.vertex1 else self.vertex1
+    def other(self, other: Vertex):
+        return self.start if other == self.end else self.end
 
 
 class Graph:
-
-    def __init__(self, data: Dict[str, float] | List[Edge], separator: str = "-"):
-        """
-        The edges of the graph are the keys of the dict object and the values are the weights
-        The edges are two vertices separated by a single char separator like eg dash "-".
-        :param data: A Dict Object
-        """
-        self.separator = separator
-        self.graph = [Edge(*item[0].split(self.separator), item[1]) for item in data.items()] if isinstance(data, Dict) else data
+    def __init__(self, data: dict = None):
+        self.data = data
+        self.vertices = list()
+        self.edges = list()
+        self.graph: dict[Vertex, dict[Vertex, Edge]] = {}
 
     def __len__(self):
-        return self.size
+        return len(self.vertices)
 
-    def __getitem__(self, item):
-        if isinstance(item, slice):
-            return self.__class__(self.graph[item])
-        return self.graph[item]
+    def __contains__(self, item):
+        item = Vertex(item) if not isinstance(item, Vertex) else item
+        return item in self.vertices
+
+    def __getitem__(self, item: str | Vertex) -> dict[Vertex, Edge]:
+        key = Vertex(item) if not isinstance(item, Vertex) else item
+        value = self.graph.get(key, None)
+        if value is None:
+            raise KeyError(f"Vertex {key} not found in graph")
+        return value
+
+    def __setitem__(self, vertex: Vertex, edge: Edge):
+        self.graph[vertex] = self.graph.get(vertex, {}) | {edge.other(vertex): edge}
+        self.vertices.append(vertex) if vertex not in self.vertices else ...
+        self.edges.append(edge) if edge not in self.edges else ...
 
     def __iter__(self):
-        return (edge for edge in self.graph)
-
-    def __add__(self, other):
-        if isinstance(other, self.__class__):
-            return self.graph + other.graph
-
-        if isinstance(other, Edge):
-            return self.graph + [other]
-
-        raise TypeError(f"{type(other)} can not be added to a graph")
-
-    def __iadd__(self, other):
-        if isinstance(other, self.__class__):
-            self.graph.extend(other.graph)
-            return self
-
-        if isinstance(other, Edge):
-            self.graph.append(other)
-            return self
-
-        raise TypeError(f"{type(other)} can not be added to a graph")
-
-    def sort(self, key=lambda edge: edge.weight, reverse=False):
-        self.graph.sort(key=key, reverse=reverse)
-
-    def adjacency_list(self):
-        adj_list = {vertex: [] for vertex in self.vertices}
-        for edge in self:
-            adj_list[edge.vertex1].append(edge.vertex2)
-            adj_list[edge.vertex2].append(edge.vertex1)
-        return adj_list
-
-    def adjacency_list_edges(self):
-        adj_list = {vertex: [] for vertex in self.vertices}
-        for edge in self:
-            adj_list[edge.vertex1].append(edge)
-            adj_list[edge.vertex2].append(edge)
-        return adj_list
-
-    @property
-    def size(self):
-        return len(self.graph)
+        return iter(self.vertices)
 
     @property
     def order(self):
         return len(self.vertices)
 
     @property
-    def cost(self):
-        return sum(edge.weight for edge in self)
+    def size(self):
+        return len(self.edges)
 
     @property
-    def vertices(self) -> List[str]:
-        vertices = set()
-        for edge in self.graph:
-            vertices.update(edge.vertices)
-        return list(vertices)
+    def weight(self):
+        """
+        Returns:
+            float: Weight of an MST as the sum of all the edges
+        """
+        return sum(edge.weight for edge in self.edges)
+
+    def build(self):
+        """
+        Build the graph from a dictionary with the below structure:
+        {
+            'v1': [('v2', 3)],
+            'v2': [('v1', 3)]
+        }
+        """
+        for vertex, neighbours in self.data.items():
+            start = Vertex(vertex)
+            for neighbour in neighbours:
+                end = Vertex(neighbour[0])
+                edge = Edge(start=start, end=end, weight=neighbour[1])
+                self[start] = edge
+
+    def build_edges(self):
+        """
+        Build from a dictionary of edges as shown below:
+        {'v1-v2': 3}
+        """
+        for key, value in self.data.items():
+            v1, v2 = key.split('-')
+            start, end = Vertex(v1), Vertex(v2)
+            edge = Edge(start=start, end=end, weight=value)
+            self[start] = edge
+            self[end] = edge
+
+    def kruskal(self):
+        union = UnionFind(self.vertices)
+        mst = Graph()
+        self.edges.sort()
+        for edge in self.edges:
+            find = union.union_find(edge.start, edge.end)
+            if find:
+                mst[edge.start] = edge
+                mst[edge.end] = edge
+        return mst
+
+    def prim(self):
+        mst = Graph()
+        mst.vertices.append(self.vertices[0])
+        while mst.order < self.order:
+            edge = min([edge for vertex in mst.vertices for neighbour, edge in self[vertex].items()
+                        if edge not in mst.edges and neighbour not in mst.vertices])
+            mst[edge.start] = edge
+            mst[edge.end] = edge
+        return mst
+
+    def show(self):
+        print('Total Weight: ', self.weight)
+        pp(self.graph)
+
+
+class UnionFind:
+    def __init__(self, vertices):
+        self.data = [{vertex} for vertex in vertices]
+
+    def union(self, set1, set2):
+        self.data.remove(set1)
+        self.data.remove(set2)
+        self.data.append(set1 | set2)
+
+    def find(self, vertex):
+        for component in self.data:
+            if vertex in component:
+                return component
+
+    def union_find(self, vertex1, vertex2):
+        set1 = self.find(vertex1)
+        set2 = self.find(vertex2)
+        if set1 == set2:
+            return False
+        self.union(set1, set2)
+        return True
+
+
+# A graph of edges and there associated weights
+graph = {'1-2': 853.40, '2-3': 160, '2-23': 933.9, '3-4': 170, '3-5': 158.7, '5-6': 142.8, '4-8': 256.7, '6-7': 68.7,
+         '7-8': 27.2, '8-9': 142.7, '7-10': 36.8, '10-11': 27.2, '8-11': 36.8, '11-12': 126, '12-13': 208.2,
+         '12-14': 363.6,
+         '14-15': 118, '13-16': 328.2, '15-16': 150, '14-17': 100, '15-18': 100, '18-19': 100, '18-17': 118,
+         '16-19': 115,
+         '20-21': 166.4, '18-21': 152.8, '21-26': 170, '26-25': 84.2, '26-27': 130, '27-28': 170, '28-35': 115,
+         '35-34': 170, '27-34': 115, '34-36': 235, '36-33': 72, '33-26': 574, '25-24': 140, '24-23': 100, '23-22': 100,
+         '22-31': 371.35, '24-30': 100, '23-29': 100, '29-30': 100, '29-32': 170, '32-33': 285, '32-31': 100,
+         '17-20': 115}
+
+graph = Graph(data=graph)
+graph.build_edges()  # use build edges
+# compute mst with kruskal
+mst = graph.kruskal()
+# show the weight and the resulting graph
+mst.show()
+
+# using prim
+mst = graph.prim()
+# show the weight and the resulting graph
+mst.show()
+
+# another type of graph
+graph1 = {
+    0: [(1, 4), (7, 8), ],
+    1: [(0, 4), (7, 11), (2, 8)],
+    2: [(1, 8), (8, 2), (5, 4), (3, 7)],
+    3: [(2, 7), (4, 9), (5, 14)],
+    4: [(3, 9), (5, 10)],
+    5: [(2, 4), (3, 14), (6, 2), (4, 10)],
+    6: [(7, 1), (5, 2), (8, 6)],
+    7: [(0, 8), (1, 11), (8, 7), (6, 1)],
+    8: [(2, 2), (6, 6), (7, 7)]
+}
+
+graph1 = Graph(data=graph1)
+# build with build method
+graph1.build()
+
+# solve mst with prim and kruskal
+prim_mst = graph1.prim()
+prim_mst.show()
+
+kruskal_mst = graph1.kruskal()
+kruskal_mst.show()
